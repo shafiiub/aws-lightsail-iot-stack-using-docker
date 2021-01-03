648ab0e2715c56175227da14fef19cd0a5e0003d
Abu Shafi <shafiiub@gmail.com>
2021-01-03 16:48:05 +1100

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -7,18 +7,18 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose.yml
+curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/shafiiub/aws-lightsail-iot-stack-using-docker/main/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
-curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
+curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/shafiiub/aws-lightsail-iot-stack-using-docker/main/docker-compose-app.service
 systemctl enable docker-compose-app
 
 # start up the application via docker-compose
