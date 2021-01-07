12ea1956b54680f5613de51ecb55f66b2871a1dc
Abu Shafi <shafiiub@gmail.com>
2021-01-08 00:20:20 +1100

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -6,6 +6,9 @@ curl -sSL https://get.docker.com | sh
 # make it so you don't need to sudo to run docker commands
 sudo usermod -aG docker ubuntu
 
+# install node.js for node red
+sudo apt install build-essential git
+
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
